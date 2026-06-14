11fdc1126eadf815989f63decb14b24b365cc004
ranasamehlotfyhefny@gmail.com <New_Life@2024>
2026-06-14 08:07:32 +0300

    base and login updates

--- src/test/java/tests/BaseTests.java
+++ src/test/java/tests/BaseTests.java
@@ -4,15 +4,15 @@ import core.DriverManager;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
 
 import java.net.MalformedURLException;
 import java.time.Duration;
 
 public class BaseTests {
 protected AppiumDriver driver;
+    SoftAssert soft=new SoftAssert();
 @BeforeClass
     public void setUp() throws MalformedURLException {
     driver= DriverManager.initializeDriver("ios");
@@ -20,9 +20,19 @@ protected AppiumDriver driver;
     WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(60));
 
 
+
 }
 
-
+@BeforeTest
+        public  void beforeeachTest()
+{
+System.out.println("beforeeachclassTest turn on your mobile");
+}
+/*    @AfterTest
+    public  void aftereachTest()
+    {
+        System.out.println("beforeeachclassTest turn off your mobile");
+    }*/
 //@AfterClass
 //    public void treatdown()
 //{
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -4,7 +4,10 @@ import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.options.XCUITestOptions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.AuthPage;
 
 import java.net.MalformedURLException;
@@ -14,19 +17,30 @@ import java.time.Duration;
 public class LoginTests extends BaseTests{
 
     private AuthPage auth;
+    @BeforeSuite
+    public void beforesuite()
+    {
+        System.out.printf("Before test Suite");
+    }
 
     @BeforeClass
     public void init()
     {
         auth=new AuthPage(driver);
     }
+
+
+    @Parameters({"platformName"})
     @Test
-    public void signin()
+    public void signin(String plat)
     {
         auth.pressloginFromHomeScreen();
         auth.signIn("ranatest@gmail.com","Aa@12345");
         boolean alertShown = auth.isSuccessAlertDisplayed();
         System.out.println("Success alert displayed: " + alertShown);
-        Assert.assertTrue(alertShown, "Success alert should be displayed after login");
+        soft.assertTrue(alertShown, "Success alert should be displayed after login");
+        soft.assertAll();
+        System.out.println(plat);
+
     }
 }
